4df44293705f339d21eb174e301b8f6452f19b94
jjq <cz_jjq@163.com>
2017-06-15 16:00:07 +0800

    add switch component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
 import Select2 from './select2';
 import Chosen from './chosen';
+import Switch from './switch';
 
 export default {
     "Select2" : Select2,
-    "Chosen" : Chosen
+    "Chosen" : Chosen,
+    "Switch" : Switch
 }
---
+++ src/lib/switch.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import $ from 'jquery';
+
+class Switch extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e){
+        this.props.onChange && this.props.onChange(e.target.checked);
+    }
+
+    componentDidMount(){
+        this.$el = $(this.el);
+        this.$el.bootstrapSwitch();
+    }
+
+    componentWillUnmount(){
+        this.$el.bootstrapSwitch('destroy');
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="checkbox" ref={ el => this.el = el} defaultChecked={this.props.checked}/>
+            </div>
+        );
+    }
+}
+
+export default Switch;
