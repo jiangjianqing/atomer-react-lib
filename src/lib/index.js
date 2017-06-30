e4a3d9aedaed2291726ace3eacd4b9682d829bb7
jjq <cz_jjq@163.com>
2017-06-30 10:21:50 +0800

    add bootstrap-slider component

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,11 @@
 import Select2 from './select2';
 import Chosen from './chosen';
 import Switch from './switch';
+import Slider from './slider';
 
 export default {
-    "Select2" : Select2,
-    "Chosen" : Chosen,
-    "Switch" : Switch
+    Select2,
+    Chosen,
+    Switch,
+    Slider
 }
