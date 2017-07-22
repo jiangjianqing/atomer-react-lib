9c02c0a70ee6aa9c81967b57b64dfdbef604908d
jjq <cz_jjq@163.com>
2017-07-22 09:23:23 +0800

    upgrade

--- src/lib/checkbox.jsx
+++ src/lib/checkbox.jsx
@@ -5,8 +5,6 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import $ from 'jquery';
 
-import style from './style.css';
-
 //checkbox和radio是input元素里两个非常特殊的type,通常使用过程中和label文字搭配，但通常会出现左右边距对不齐的问题
 //这里使用了bootstrap对checkbox 和 radio的标准设置 ,见深入理解bootstrap p68
 
